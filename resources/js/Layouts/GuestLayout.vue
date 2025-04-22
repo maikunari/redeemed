af8ec620201a24bd7ba458d134b3cedbe7b6c30c
maikunari <maikunari@gmail.com>
2025-04-22 17:36:48 +0900

    Update redeem text styling in guest layout to match header

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -9,8 +9,13 @@ const version = computed(() => usePage().props.version);
 <template>
     <div class="min-h-screen relative">
         <slot />
-        <div class="absolute bottom-4 right-4 text-sm text-gray-400">
-            v{{ version }}
+        <div class="absolute bottom-4 right-4 text-gray-400 flex flex-col items-end">
+            <div class="text-3xl font-black tracking-tight font-['Roboto'] italic drop-shadow-sm">redeem</div>
+            <div class="text-sm">v{{ version }}</div>
         </div>
     </div>
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,900&display=swap');
+</style>
