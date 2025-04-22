003a041db7373d933b45261abb4a3d918c896b98
maikunari <maikunari@gmail.com>
2025-04-22 17:53:36 +0900

    Update footer to be positioned absolutely at the bottom

--- resources/js/Components/AppFooter.vue
+++ resources/js/Components/AppFooter.vue
@@ -1,6 +1,9 @@
 <template>
-    <footer class="mt-auto py-4 text-right text-sm text-gray-500 px-8">
-        <span>redeem v{{ version }}</span>
+    <footer class="absolute bottom-0 w-full py-4 px-8">
+        <div class="flex flex-col items-end text-gray-400">
+            <div class="text-3xl font-black tracking-tight font-['Roboto'] italic drop-shadow-sm">redeem</div>
+            <a href="https://github.com/maikunari/redeemed" target="_blank" rel="noopener noreferrer" class="text-sm hover:text-gray-600 transition-colors">v{{ version }}</a>
+        </div>
     </footer>
 </template>
 
@@ -11,4 +14,8 @@ defineProps({
         required: true
     }
 });
-</script> 
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,900&display=swap');
+</style> 
