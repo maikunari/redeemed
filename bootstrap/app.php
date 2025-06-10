6221670c555d73ef8cad6ca9d612601ec2214b85
maikunari <maikunari@gmail.com>
2025-06-10 11:10:16 +0900

    Troubleshoot login and undo changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,8 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    
+    /*
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
@@ -18,6 +20,16 @@ return Application::configure(basePath: dirname(__DIR__))
 
         //
     })
+*/
+->withMiddleware(function (Middleware $middleware) {
+    $middleware->web(append: [
+        \App\Http\Middleware\HandleInertiaRequests::class,
+        \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+    ]);
+
+    //
+})
+
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
