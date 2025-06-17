a61fb2b20dea88ba34a88348dfdd90aee0bf9774
maikunari <maikunari@gmail.com>
2025-06-17 11:08:03 +0900

    UI polish: center spinner on download screen, style 'Enter Another Code' as text link on success

--- resources/js/Pages/Redeem/Form.vue
+++ resources/js/Pages/Redeem/Form.vue
@@ -106,8 +106,8 @@
 
                                 <div v-if="downloadStarted && isLoading">
                                     <!-- Spinner and 'Preparing your download...' message -->
-                                    <div class="p-6 bg-green-50 rounded-lg text-center">
-                                        <svg class="animate-spin w-16 h-16 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                    <div class="p-6 bg-green-50 rounded-lg text-center flex flex-col items-center justify-center">
+                                        <svg class="animate-spin w-16 h-16 text-indigo-600 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                             <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                                             <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                                         </svg>
@@ -124,7 +124,9 @@
                                         <h3 class="mt-4 text-lg font-medium text-green-800">Thank you!</h3>
                                         <p class="mt-2 text-sm text-green-600">Your download is ready.</p>
                                         <button @click="triggerDownload" class="mt-4 px-4 py-2 bg-indigo-600 text-white rounded">Download File</button>
-                                        <button @click="resetForm" class="mt-4 px-4 py-2 bg-gray-200 text-gray-700 rounded">Enter Another Code</button>
+                                        <div class="mt-4">
+                                            <button @click="resetForm" class="text-indigo-600 hover:text-indigo-800 underline bg-transparent border-none p-0 shadow-none cursor-pointer">Enter Another Code</button>
+                                        </div>
                                     </div>
                                 </div>
 
